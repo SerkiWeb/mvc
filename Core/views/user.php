4e24718ffdaf6a03dae755a67ca8ebe3384f10f2
karim <kturki85@gmail.com>
2019-07-09 17:15:25 +0200

    feat: display profil picture

--- Core/views/user.php
+++ Core/views/user.php
@@ -1,19 +1,23 @@
-<div>
+<div class="user">
 	<h1 class="bg-info">Mon profil</h1>
 	<p class="text-info">
 		vos informations personnelles
 	</p> 
-<table class="table table-striped">
-	<thead>
-		<tr>
-			<th>Nom</th>
-			<th>password</th>
-		</tr>
-	</thead>
+<table class="table">
 	<tbody>
 		<tr>
-			<td><?php echo($user['nom']); ?></td>
-			<td><?php echo($user['password']); ?></td>
+			<td>nom</td>
+			<td><input class="form-control" type="text" name="nom" value="<?php echo($user['nom']); ?>" readonly></td>		
+		</tr>
+		<tr>
+			<td class="col-md-2">password</td>
+			<td class="col-md-9">
+				<input class="form-control" type="password" name="password" value="<?php echo($user['password']); ?>" readonly>
+			</td>
+		</tr>
+		<tr>
+			<td>photo</td>
+			<td><img src="uploads/"<?php echo($user['nom_photo']) ?> ></td>
 		</tr>
 	</tbody>
 </table>
